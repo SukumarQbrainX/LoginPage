7b98df2e52551e0debb1c1f044b50fc6fb7e3bed
Sukum <Sukum@LAPTOP-F0GMDFRG>
2021-01-15 13:37:57 +0530

    Added a class file

---
+++ src/com/JDBC/DATA.java
@@ -0,0 +1,18 @@
+package com.JDBC;
+import java.sql.*;
+
+public class DATA  {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "DATABASE1", "admin");
+		Statement S1 = con.createStatement();
+		ResultSet res = S1.executeQuery("select * from students");
+		while ( res.next()) { 
+			
+			System.out.println(res.getString(1)+ " " +res.getString(2));
+		}
+		
+		con.close();
+	}
+
+}
---
+++ src/com/JDBC/getset.java
@@ -0,0 +1,15 @@
+package com.JDBC;
+
+public class getset {
+	private int values;
+
+	public int getValues() {
+		return values;
+	}
+
+	public void setValues(int values) {
+		this.values = values;
+	}
+	
+
+}
